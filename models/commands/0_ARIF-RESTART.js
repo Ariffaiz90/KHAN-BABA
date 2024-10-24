06092d8335b03b2fdc66d4122edb614d3b3457df
bk3264071 <32306831-bk3264071@users.noreply.replit.com>
2024-10-24 09:04:22 +0000

    Initial commit

---
+++ models/commands/0_ARIF-RESTART.js
@@ -0,0 +1,15 @@
+module.exports.config = {
+  name: "restart",
+  version: "1.0.0",
+  hasPermssion: 2,
+  credits: "ARIF-BABU",
+  description: "Restart Bot",
+  commandCategory: "system",
+  usages: "",
+  cooldowns: 5
+};
+
+module.exports.run = async ({ api, event, args }) => {
+  const { threadID, messageID } = event;
+  return api.sendMessage(`आरिफ़ बाबू 2 मिनट रुको रीस्टार्ट कर रहा हूं 😛🍎`, threadID, () => process.exit(1));
+}
