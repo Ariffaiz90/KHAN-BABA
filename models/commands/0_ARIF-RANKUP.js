9b3d6f5714dbc924cbf3cb7f85170f0358c6d08f
Ariffaiz90 <payalthakur969491@gmail.com>
2024-10-29 15:46:39 +0530

    Update 0_ARIF-RANKUP.js

--- models/commands/0_ARIF-RANKUP.js
+++ models/commands/0_ARIF-RANKUP.js
@@ -77,7 +77,7 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
     let ctx = canvas.getContext("2d");
     ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
     ctx.rotate(-25 * Math.PI / 180);
-    ctx.drawImage(baseAvt1, 40, 710, 630, 700);
+    ctx.drawImage(baseAvt1, 40, 200, 200, 200);
     const imageBuffer = canvas.toBuffer();
     fs.writeFileSync(pathImg, imageBuffer);
     fs.removeSync(pathAvt1);
